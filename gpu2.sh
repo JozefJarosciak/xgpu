c4257381fa954c55a5fe4c5ff9e2d894c96d7340
Jozef Jaroščiak <jarosciak@gmail.com>
2023-10-02 21:29:24 -0400

    Update gpu2.sh

--- gpu2.sh
+++ gpu2.sh
@@ -16,7 +16,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x0a6969fff003b760c97005e03ff5a9741126167a/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
