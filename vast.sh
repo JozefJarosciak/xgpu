e3c359be4a148c1ba12dc76c3f7f104e7a2c09e2
Jozef Jaroščiak <jarosciak@gmail.com>
2023-10-03 06:49:52 -0400

    Create vast.sh

---
+++ vast.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Update package list and install necessary packages
+apt update
+apt -y install ocl-icd-opencl-dev
+apt -y install nano
+apt -y install htop
+apt -y install nvtop
+apt -y install cmake
+apt -y install python3-pip
+
+# Clone the repository and build the project
+git clone https://github.com/shanhaicoder/XENGPUMiner.git
+cd XENGPUMiner
+chmod +x build.sh
+./build.sh
+
+# Update the configuration file
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+
+# Install Python requirements
+pip install -U -r requirements.txt
+nohup python miner.py --gpu=true > miner.log 2>&1 &
+nohup ./xengpuminer > xengpuminer.log 2>&1 &
