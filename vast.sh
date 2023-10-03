17861f795221b01c7b2cd7ff00a384cf068fb3b2
Jozef Jaroščiak <jarosciak@gmail.com>
2023-10-03 06:50:45 -0400

    Update vast.sh

--- vast.sh
+++ vast.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Update package list and install necessary packages
-apt update
-apt -y install ocl-icd-opencl-dev
-apt -y install nano
-apt -y install htop
-apt -y install nvtop
-apt -y install cmake
-apt -y install python3-pip
+sudo apt update
+sudo apt -y install ocl-icd-opencl-dev
+sudo apt -y install nano
+sudo apt -y install htop
+# sudo apt -y install nvtop
+sudo apt -y install cmake
+sudo apt -y install python3-pip
 
 # Clone the repository and build the project
 git clone https://github.com/shanhaicoder/XENGPUMiner.git
@@ -19,6 +19,6 @@ chmod +x build.sh
 sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
 
 # Install Python requirements
-pip install -U -r requirements.txt
-nohup python miner.py --gpu=true > miner.log 2>&1 &
-nohup ./xengpuminer > xengpuminer.log 2>&1 &
+sudo pip install -U -r requirements.txt
+sudo nohup python miner.py --gpu=true > miner.log 2>&1 &
+sudo nohup ./xengpuminer > xengpuminer.log 2>&1 &
