c449667fed5ae1d0a1349b813ec5c38a5f9deef8
Ajdin Dracic <53024321+dirrrtyjesus@users.noreply.github.com>
2023-10-09 10:34:07 -0400

    runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -16,7 +16,7 @@ chmod +x build.sh
 ./build.sh
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x4244A1743BD57A3dbe2aa7914aA0a287437A0EFb/g' config.conf
 
 # Install Python requirements
 pip install -U -r requirements.txt
