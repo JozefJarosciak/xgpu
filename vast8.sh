2fa290832485afc487fb5a13cbd606f97280f2c2
Jozef Jaroščiak <jarosciak@gmail.com>
2023-11-03 06:01:35 -0400

    Update vast8.sh

--- vast8.sh
+++ vast8.sh
@@ -5,7 +5,6 @@ echo "       XENBLOCKS         "
 echo "    STARTING INSTALL     "
 echo "-------------------------"
 
-sudo apt update > /dev/null 2>&1
 sudo apt -y upgrade > /dev/null 2>&1
 echo "STEP 1 of 10: Completed Packages Update"
 
@@ -37,13 +36,21 @@ echo "STEP 9 of 10: Installed Python Requirements"
 
 echo "STEP 10 of 10: Starting Miner & GPU"
 sudo nohup python3 miner.py --gpu=true > miner.log 2>&1 &
+sleep 1
 sudo nohup ./xengpuminer -d0 > xengpuminer-0.log 2>&1 &
+sleep 1
 sudo nohup ./xengpuminer -d1 > xengpuminer-1.log 2>&1 &
+sleep 1
 sudo nohup ./xengpuminer -d2 > xengpuminer-2.log 2>&1 &
+sleep 1
 sudo nohup ./xengpuminer -d3 > xengpuminer-3.log 2>&1 &
+sleep 1
 sudo nohup ./xengpuminer -d4 > xengpuminer-4.log 2>&1 &
+sleep 1
 sudo nohup ./xengpuminer -d5 > xengpuminer-5.log 2>&1 &
+sleep 1
 sudo nohup ./xengpuminer -d6 > xengpuminer-6.log 2>&1 &
+sleep 1
 sudo nohup ./xengpuminer -d7 > xengpuminer-7.log 2>&1 &
 
 echo "-------------------------"
