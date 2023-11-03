535112c7a220a41f715be1896bc1b481e6f7110d
Jozef Jaroščiak <jarosciak@gmail.com>
2023-11-03 06:02:03 -0400

    Update vast4.sh

--- vast4.sh
+++ vast4.sh
@@ -5,7 +5,6 @@ echo "       XENBLOCKS         "
 echo "    STARTING INSTALL     "
 echo "-------------------------"
 
-sudo apt update > /dev/null 2>&1
 sudo apt -y upgrade > /dev/null 2>&1
 echo "STEP 1 of 10: Completed Packages Update"
 
@@ -29,7 +28,7 @@ sudo chmod +x build.sh > /dev/null 2>&1
 sudo ./build.sh > /dev/null 2>&1
 echo "STEP 7 of 10: Permissions set!"
 
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf > /dev/null 2>&1
+sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf > /dev/null 2>&1
 echo "STEP 8 of 10: Replaced ETH address"
 
 sudo pip install -U -r requirements.txt > /dev/null 2>&1
@@ -37,9 +36,13 @@ echo "STEP 9 of 10: Installed Python Requirements"
 
 echo "STEP 10 of 10: Starting Miner & GPU"
 sudo nohup python3 miner.py --gpu=true > miner.log 2>&1 &
+sleep 1
 sudo nohup ./xengpuminer -d0 > xengpuminer-0.log 2>&1 &
+sleep 1
 sudo nohup ./xengpuminer -d1 > xengpuminer-1.log 2>&1 &
+sleep 1
 sudo nohup ./xengpuminer -d2 > xengpuminer-2.log 2>&1 &
+sleep 1
 sudo nohup ./xengpuminer -d3 > xengpuminer-3.log 2>&1 &
 
 echo "-------------------------"
