659a2e99c19079a6528fe3027395b987c3fe18f9
Jozef Jaroščiak <jarosciak@gmail.com>
2023-11-03 06:00:52 -0400

    Update vast12.sh

--- vast12.sh
+++ vast12.sh
@@ -5,7 +5,6 @@ echo "       XENBLOCKS         "
 echo "    STARTING INSTALL     "
 echo "-------------------------"
 
-sudo apt update > /dev/null 2>&1
 sudo apt -y upgrade > /dev/null 2>&1
 echo "STEP 1 of 10: Completed Packages Update"
 
