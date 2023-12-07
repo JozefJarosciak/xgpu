e4ae2ce85ba1ea53d5821dc101bb1d94b8358c37
fhuudeefhod <147883006+fhuudeefhod@users.noreply.github.com>
2023-12-07 23:33:59 +0300

    Update vast4.sh

--- vast4.sh
+++ vast4.sh
@@ -28,7 +28,7 @@ sudo chmod +x build.sh > /dev/null 2>&1
 sudo ./build.sh > /dev/null 2>&1
 echo "STEP 7 of 10: Permissions set!"
 
-sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf > /dev/null 2>&1
+sudo sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0x20201cbc70b56352e54f55290524938be43c75b2/g' config.conf > /dev/null 2>&1
 echo "STEP 8 of 10: Replaced ETH address"
 
 sudo pip install -U -r requirements.txt > /dev/null 2>&1
